2ab79032fb771e221faf37c30cec70427ab16ccf
jtanza <johntanza@gmail.com>
2021-12-10 09:04:57 -0500

    adds day one part two

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -3,10 +3,18 @@ module Main where
 main :: IO ()
 main = do
     input <- readFile "input/one.txt"
-    print $ dayOne input
+    let dayOneInput = map (read :: String->Int) (lines input) 
+    print $ dayOne dayOneInput
+    print $ dayOnePartTwo dayOneInput
 
--- |'dayOne' counts pairwise elements in our `input` that are strictly increasing in value
-dayOne :: Num a => [Char] -> a
-dayOne input = foldl (\x y -> if (snd y) > (fst y) then x + 1 else x) 0 (zip xs (tail xs))
-  where xs = map (read :: String->Int) (lines input)
+-- |'dayOne' counts pairwise elements in our 'input' that are strictly increasing in value
+dayOne :: (Ord b, Num a) => [b] -> a
+dayOne xs = foldl (\x y -> if (snd y) > (fst y) then x + 1 else x) 0 (zip xs (tail xs))
 
+partition :: Int -> [a] -> [[a]]
+partition _ [] = []
+partition n xs = (take n xs) : (partition n (tail xs))
+
+-- |sums triplets in 'input' before counting consecutive pairs that are strictly increasing in value
+dayOnePartTwo :: (Ord b, Num b, Num a) => [b] -> a
+dayOnePartTwo input = dayOne $ map sum (partition 3 input)
