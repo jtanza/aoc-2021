5f19043e45626bc4a8f69253cb3d75b94fc57110
jtanza <johntanza@gmail.com>
2021-12-10 20:18:07 -0500

    adds input format function

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -3,18 +3,22 @@ module Main where
 main :: IO ()
 main = do
     input <- readFile "input/one.txt"
-    let dayOneInput = map (read :: String->Int) (lines input) 
+    let dayOneInput = formatInput (read :: String->Int) input
     print $ dayOne dayOneInput
     print $ dayOnePartTwo dayOneInput
 
+-- | applies 'f' to each line of our puzzle input, formating it for our solution function
+formatInput :: (String -> a) -> String -> [a]
+formatInput f input = map f (lines input)
+
 -- |'dayOne' counts pairwise elements in our 'input' that are strictly increasing in value
-dayOne :: (Ord b, Num a) => [b] -> a
-dayOne xs = foldl (\x y -> if (snd y) > (fst y) then x + 1 else x) 0 (zip xs (tail xs))
+dayOne :: (Ord a, Num b) => [a] -> b
+dayOne xs = foldl (\count pair -> if (snd pair) > (fst pair) then count + 1 else count) 0 (zip xs (tail xs))
 
 partition :: Int -> [a] -> [[a]]
 partition _ [] = []
 partition n xs = (take n xs) : (partition n (tail xs))
 
 -- |sums triplets in 'input' before counting consecutive pairs that are strictly increasing in value
-dayOnePartTwo :: (Ord b, Num b, Num a) => [b] -> a
+dayOnePartTwo :: (Ord a, Num a, Num b) => [a] -> b
 dayOnePartTwo input = dayOne $ map sum (partition 3 input)
