df26772e8de84d65eed6edac76258c39c537c4bc
jtanza <johntanza@gmail.com>
2021-12-30 13:24:38 -0500

    updates

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -2,16 +2,27 @@ module Main where
 
 main :: IO ()
 main = do
-    input <- readFile "input/two.txt"
-    print $ dayTwo $ formatInput id input
+  one
+  two
 
 -- |Applies 'f' to each line of our puzzle input, formating it for our solution function
 formatInput :: (String -> a) -> String -> [a]
 formatInput f input = map f (lines input)
 
+---------------
+-- Day One
+---------------
 -- |'dayOne' counts pairwise elements in our 'input' that are strictly increasing in value
+
 dayOne :: (Ord a, Num b) => [a] -> b
-dayOne xs = foldl (\count pair -> if (snd pair) > (fst pair) then count + 1 else count) 0 (zip xs (tail xs))
+dayOne xs =
+  foldl
+    (\count pair ->
+       if (snd pair) > (fst pair)
+         then count + 1
+         else count)
+    0
+    (zip xs (tail xs))
 
 -- |Sums triplets in 'input' before counting consecutive pairs that are strictly increasing in value
 dayOnePartTwo :: (Ord a, Num a, Num b) => [a] -> b
@@ -21,16 +32,44 @@ partition :: Int -> [a] -> [[a]]
 partition _ [] = []
 partition n xs = (take n xs) : (partition n (tail xs))
 
+one :: IO ()
+one = do
+  input <- readFile "input/one.txt"
+  let dayOneInput = formatInput (read :: String -> Int) input
+  print $ dayOne dayOneInput
+  print $ dayOnePartTwo dayOneInput
+
+---------------
+-- Day Two
+---------------
 -- |Given a series of commands of the form '(direction, magnitude)' returns the product of both after applying
--- all commands in the sequence to the starting position '(0,0)'
+-- all commands in the sequence to the starting position '(0,0) '
 dayTwo :: (Num a, Read a) => [String] -> a
 dayTwo input = horizontalPos * depth
-  where (horizontalPos, depth) = foldl (\coord command -> parseCommand command coord) (0,0) $ map words input
+  where
+    (horizontalPos, depth) =
+      foldl (\coord command -> parseCommand command coord) (0, 0) $
+      map words input
 
 parseCommand :: (Num a, Num b, Read a, Read b) => [String] -> (a, b) -> (a, b)
-parseCommand xs coord  = case xs of
-                  ("forward":n:_) -> (x  + read n, y)
-                  ("down":n:_)    -> (x, (y + read n))
-                  ("up":n:_)      -> (x, (y - read n))
-  where (x, y) = coord
-  
+parseCommand xs coord =
+  case xs of
+    ("forward":n:_) -> (x + read n, y)
+    ("down":n:_) -> (x, (y + read n))
+    ("up":n:_) -> (x, (y - read n))
+  where
+    (x, y) = coord
+
+two :: IO ()
+two = do
+  dayTwoInput <- readFile "input/two.txt"
+  print $ dayTwo $ formatInput id dayTwoInput
+
+---------------
+-- Day Three
+---------------
+interleave :: [[a]] -> [[a]]
+interleave [] = []
+interleave ([]:_) = []
+interleave xs = map head xs : interleave (map tail xs)
+
